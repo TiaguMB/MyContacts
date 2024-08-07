99f789a193c774876e8d70cf2125942e720c30c3
TiaguMB <tiagomb.privado@gmail.com>
2024-08-07 11:35:30 -0300

    feat: add routes file and rem comments

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,24 +1,19 @@
 class ContactController {
-	index() {
-		// Listar todos os registros
+	index(request, response) {
+		response.send('Send from Contact Controller');
 	}
 
 	show() {
-		// Obter UM registro
 	}
 
 	store() {
-		// Criar novo registro
 	}
 
 	update() {
-		// Editar um registro
 	}
 
 	delete() {
-		// Deletar um registro
 	}
 }
 
-// Singleton
 module.exports = new ContactController();
