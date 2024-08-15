fa9fbc1b44befca1af84526b947dbdf2d6aa352d
TiaguMB <tiagomb.privado@gmail.com>
2024-08-15 18:51:01 -0300

    feat: add express error middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -6,6 +7,12 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+	console.log('#### Error Handler');
+	response.sendStatus(500);
+
+	next(error);
+});
 
 // eslint-disable-next-line no-console
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
